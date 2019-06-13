8d40f0e5dd2888ac5faa500e02b49e2a75e46aab
Andrés Angelini <andresangelini@users.noreply.github.com>
2019-06-13 19:49:57 -0300

    Replace multiple tasks with 'serve'

---
+++ dist/js/index.js
@@ -0,0 +1,48 @@
+// Use css pointerevent to detect IE 10 and older since these don't scale SVGs properly when used as background images.
+// For them, insert multiple divs so that each of their backgrounds can be used to display their respective layers of the
+// SVG stack.
+if (Modernizr.csspointerevents) {
+  // SVG are not supported properly
+  appendElementsToBoard('div');
+}
+
+// Use JPEG 2000 image format to detect Safari and Safari iOS (down to 4.0 and 4.3 respectively) since these are the only ones that don't support
+// SVG fragment identifiers but do support this feature.
+// Just lilke with IE 10 and older, insert multiple divs inside the board so that each of them use a single background image.
+Modernizr.on('jpeg2000', function(result) {
+  if (result) {
+    // SVG Fragment identifiers are not supported
+    appendElementsToBoard('div');
+  }
+});
+
+// Use WebP Image Format to target Chrome, Opera and derivatives only and then Passive Event
+// Listeners to detect support for SVG Fragment Identifiers. Again, insert multiple div
+// elements inside the board so that each of them use a single background.
+// image.
+if (!Modernizr.webp) {
+  if (!Modernizr.passiveeventlisteners) {
+    // SVG Fragment identifiers are not supported
+    appendElementsToBoard('div');
+  }
+}
+
+// IE < 10 don't support CSS Generated content pseudo-elements nor using innerHTML on html elements.
+if (!Modernizr.generatedcontent) {
+  // not-supported
+  document.body.appendChild(document.createTextNode("Oops! It seems your browser or device isn't able to display this site properly. Please, update it."));
+}
+
+function appendElementsToBoard(elem) {
+  // IE 8 raises an error with document.getElementsByClassName().
+  try {
+    // Insert multiple elements in every board div.
+    for (var i = 0; i < document.getElementsByClassName('board').length; i++) {
+      for (var k = 1; k < 14; k++) {
+       document.getElementsByClassName('board')[i].appendChild(document.createElement(elem));
+      }
+    }
+  } catch(err) {
+    // do nothing.
+  }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,89 @@
+/**
+ * Settings
+ * Set final names for the compiled js and css files
+ */
+
+
+const settings = {
+  scss: {
+    name: 'index',
+    suffix: 'min'
+  },
+  js: {
+    name: 'index',
+    suffix: 'min'
+  }
+}
+
+
+/**
+ * Paths to project folders
+ */
+
+const paths = {
+	input: 'src/',
+	output: 'dist/',
+	js: {
+		input: ['src/js/modernizr.js', 'src/js/index.js'],
+		output: 'dist/js/'
+	},
+	scss: {
+		input: 'src/scss/**/*.scss',
+		output: 'dist/css/'
+	},
+	svgs: {
+		input: 'src/svg/**/*.svg',
+		output: 'dist/svg/'
+	},
+	html: {
+		input: 'src/**/*',
+		output: 'dist/'
+	},
+	reload: './dist/'
+};
+
+
+/**
+ * Project dependencies
+ */
+
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const modernizr = require('gulp-modernizr');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const {series} = require('gulp');
+const {parallel} = require('gulp');
+const del = require('del');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync');
 
+// Styles
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const modernizr = require('gulp-modernizr');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Scripts
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+
+/**
+ * Gulp Tasks
+ */
+
 // Copy all HTML files
 function copyHtml() {
-  return gulp.src('src/**/*.html')
-    .pipe(gulp.dest('dist'));
+  return gulp.src(paths.html.input)
+    .pipe(gulp.dest(paths.html.output));
 }
 
 // Copy all SVG files
 function copySvgs() {
-  return gulp.src('src/svg/**/*.svg')
-    .pipe(gulp.dest('dist/svg'));
+  return gulp.src(paths.svgs.input)
+    .pipe(gulp.dest(paths.svgs.output));
 }
 
 // Compile and minify Sass Scss files
 function compileScss() {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src(paths.scss.input)
     .pipe(sourcemaps.init())
     .pipe(sass({
       errorLogToConsole: true,
@@ -32,37 +93,44 @@ function compileScss() {
 
       cascade: false
     }))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({suffix: '.' + settings.scss.suffix}))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest(paths.scss.output))
     .pipe(browserSync.stream());
 }
 
 // Compile and minify JS files
 function compileJs() {
-  return gulp.src(['src/js/modernizr.js', 'src/js/index.js'])
-    .pipe(concat('index.js'))
+  return gulp.src(paths.js.input)
+    .pipe(concat(settings.js.name + '.js'))
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(rename({suffix: '.' + settings.js.suffix}))
+    .pipe(gulp.dest(paths.js.output));
+}
+
+// Delete output folder
+function clean() {
+  return del([output]);
 }
 
 // Update files and reload browser when there are changes in the source files
 function watch() {
   browserSync.init({
     server: {
-      baseDir: 'dist'
+      baseDir: paths.reload
     }
   });
-  gulp.watch('src/**/*.html', copyHtml);
-  gulp.watch('src/**/*.svg', copySvgs);
-  gulp.watch('src/scss/**/*.scss', compileScss);
-  gulp.watch('src/js/**/*.js', compileJs);
-  gulp.watch('src/**/*').on('change', browserSync.reload)
+
+  gulp.watch(paths.html.input, copyHtml);
+  gulp.watch(paths.svgs.input, copySvgs);
+  gulp.watch(paths.scss.input, compileScss);
+  gulp.watch(paths.js.input, compileJs);
+  gulp.watch(paths.input + '/**/*').on('change', browserSync.reload)
 }
 
-exports.copyHtml = copyHtml;
-exports.copySvgs = copySvgs;
-exports.compileScss = compileScss;
-exports.compileJs = compileJs;
-exports.watch = watch;
+
+/**
+ * Export Tasks
+ */
+
+exports.serve = series([parallel(copyHtml, copySvgs, compileScss, compileJs)], watch);
